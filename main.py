3c719150414b572bb976bd52145a277452a4732d
Никитка <46526694+Epsilonbolee0@users.noreply.github.com>
2019-03-07 01:22:14 +0300

    FIXED while - bug

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ root = Tk()
 
 Label(root, text = "Hello worl").pack()
 
-i = []
+i = 0
+
+# while - problem fixed
 
 while i < 15:
   i += 2
-  Label(root, text = "i").pack()
+  Label(root, text = str(i)).pack()
 
 root.mainloop()
